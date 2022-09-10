0940cf83d6f030eeb034b671efd337c4aa6a9212
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-09-10 12:05:04 +0000

    topaz: Switch to SkiaGL Threaded
    - SkiaVK works, in some cases HwUI works better, but mostly shuttery and
    microlags, so let's go for SkiaGL Threaded
    
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,9 @@ BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# HWUI
+USE_OPENGL_RENDERER := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_topaz
 TARGET_RECOVERY_DEVICE_MODULES := init_topaz
