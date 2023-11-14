f86aceaf9aa1340e4361c4053d421da9d0a4bb27
Cyber Knight <cyberknight755@gmail.com>
2023-11-15 00:28:37 +0800

    topaz: Build OMX service
    - I'm not going through the fuss to make Codec2 work on AOSP;
    - Hence, let's build OMX service instead.
    
    Change-Id: Ic1609cb632a4c0fe00addc48b1570ce30456e674
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -340,6 +340,7 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
