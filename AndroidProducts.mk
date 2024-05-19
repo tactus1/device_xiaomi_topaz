31b9878738c9c990498bcba19bc7e264ed404d5c
Tactus <142695427+tactus1@users.noreply.github.com>
2024-05-19 23:11:04 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/derp_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_topaz-user \
-    derp_topaz-userdebug \
-    derp_topaz-eng
+    derp_topaz-ap1a-user \
+    derp_topaz-ap1a-userdebug \
+    derp_topaz-ap1a-eng
